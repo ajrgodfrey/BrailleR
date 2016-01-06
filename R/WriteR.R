42cc4922e98144cfb1071625614423e520e1b643
ajrgodfrey <a.j.godfrey@massey.ac.nz>
2016-01-06 21:21:08 +1300

    some minor changes

--- R/WriteR.R
+++ R/WriteR.R
@@ -4,7 +4,7 @@
 WriteR = function(file=NULL, math = c("webTeX", "MathJax")){
 if(interactive()){
 if(.Platform$OS.type=="windows"){
-#shell.exec(system.file("Python/WriteR/WriteR.pyw", package="BrailleR"))
+shell(paste(system.file("Python/WriteR/WriteR.pyw", package="BrailleR"), ifelse(is.null(file), "", file)))
 } else{warning("This function is for users running R under the Windows operating system.\n")}
 } else{warning("This function is meant for use in interactive mode only.\n")}
 return(invisible(NULL))
