1be5e7bbc86e0dfbf84a3e7427b3e407ccc23f72
ajrgodfrey <a.j.godfrey@massey.ac.nz>
2016-01-31 06:25:07 +1300

    changes for v0.23.9

--- R/WriteR.R
+++ R/WriteR.R
@@ -5,6 +5,8 @@ WriteR = function(file=NULL, math = c("webTeX", "MathJax")){
 if(interactive()){
 if(.Platform$OS.type=="windows"){
 if(Sys.which("python")!=""){
+if(!is.null(file)) { if(!file.exists(file)){
+cat("Starting new file\n", file=file)}}
 shell(paste(system.file("Python/WriteR/WriteR.pyw", package="BrailleR"), ifelse(is.null(file), "", file)))
 } else{warning("This function requires an installation of Python 2.7 and wxPython 2.8.\n")}
 } else{warning("This function is for users running R under the Windows operating system.\n")}
