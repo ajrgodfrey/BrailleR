12869f5314914009b0450ec690b34702296ba21b
ajrgodfrey <a.j.godfrey@massey.ac.nz>
2016-05-31 21:33:04 +1200

    changes for v0.25.0

--- R/BrailleRUsefulLinks.R
+++ R/BrailleRUsefulLinks.R
@@ -6,9 +6,14 @@ browseURL("http://R-Resources.massey.ac.nz/BrailleR/")
 return(invisible(NULL))
 }
 
-LURN = function(){
+LURN = function(BlindVersion = getOption("BrailleR.VI")){
 if(interactive()){
+if(BlindVersion){
+browseURL("http://R-Resources.massey.ac.nz/LURNBlind/front.html")
+}
+else{
 browseURL("http://R-Resources.massey.ac.nz/LURN/front.html")
+}
 } else{warning("This function is meant for use in interactive mode only.\n")}
 return(invisible(NULL))
 }
