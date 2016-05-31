a850c81dc8216a801c5a8d7d306da8431231c0c7
ajrgodfrey <a.j.godfrey@massey.ac.nz>
2016-05-31 17:59:25 +1200

    changes for v0.25.0

--- R/GetWriteR.R
+++ R/GetWriteR.R
@@ -12,7 +12,7 @@ download.file("http://R-Resources.massey.ac.nz/WriteR/WriteR.zip", "WriteR.zip")
 }
 unzip("WriteR.zip")
 file.remove("WriteR.zip")
-file.rename("WriteR.exe", paste0(getOption("BrailleR.Folder"), "WriteR.exe"))
+file.copy("WriteR.exe", paste0(getOption("BrailleR.Folder"), "WriteR.exe"))
 message('The WriteR application has been added to your MyBrailleR folder.\nIt is assumed you wanted to download this file by issuing the last command.\nYou can delete "WriteR.exe" at any time to remove WriteR from your system.\n')
 }
 } else{warning("This function is for users running R under the Windows operating system.\n")}
