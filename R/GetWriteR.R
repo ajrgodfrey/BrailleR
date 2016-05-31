22def5d495b007574b2a230f2848aa57850bf092
ajrgodfrey <a.j.godfrey@massey.ac.nz>
2016-05-31 18:23:35 +1200

    changes for v0.25.0

--- R/GetWriteR.R
+++ R/GetWriteR.R
@@ -13,7 +13,8 @@ message("The WriteR application has been added to your MyBrailleR folder.")
 }
 else{
 browseURL("http://R-Resources.massey.ac.nz/WriteR/WriteR.zip")
-message("The WriteR application has been downloaded but you need to move it to your MyBrailleR folder.")
+message("The WriteR application has been downloaded but you need to unzip it.")
+message("Move it to your MyBrailleR folder and unzip before use.")
 }
 message("It is assumed you wanted to download this file by issuing the last command.")
 message("You can delete WriteR.exe at any time to remove WriteR from your system.")
