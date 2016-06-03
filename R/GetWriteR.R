99e413ab9a851ae84e984af4618d64bf6625e893
ajrgodfrey <a.j.godfrey@massey.ac.nz>
2016-06-03 22:51:46 +1200

    changes for v0.25.0

--- R/GetWriteR.R
+++ R/GetWriteR.R
@@ -1,4 +1,4 @@
-
+# look out for two version of GetWriteR() second one is while the exe file is borken.
 
 GetWriteR= function(UseGitHub = TRUE){
 if(interactive()){
@@ -23,3 +23,66 @@ message("You can delete WriteR.exe at any time to remove WriteR from your system
 } else{warning("This function is meant for use in interactive mode only.\n")}
 return(invisible(NULL))
 }
+
+
+GetPandoc = function(){
+if(interactive()){
+if(.Platform$OS.type=="windows"){
+if(require(BrailleR)){
+message("This command will download a file and save it to your hard drive.\n")
+installr::install.pandoc(to_restart=FALSE, download_dir = getOption("BrailleR.Folder"), keep_install_file=TRUE)
+message("The installer file has been added to your MyBrailleR folder.")
+message("You can delete it at any time, but that will not uninstall the application.")
+}
+} else{warning("This function is for users running R under the Windows operating system.\n")}
+} else{warning("This function is meant for use in interactive mode only.\n")}
+return(invisible(NULL))
+}
+
+Get7zip = function(){
+if(interactive()){
+if(.Platform$OS.type=="windows"){
+if(require(BrailleR)){
+message("This command will download a file and save it to your hard drive.")
+installr::install.7zip(download_dir = getOption("BrailleR.Folder"), keep_install_file=TRUE)
+message("The installer file has been added to your MyBrailleR folder.")
+message("You can delete it at any time, but that will not uninstall the application.")
+}
+} else{warning("This function is for users running R under the Windows operating system.\n")}
+} else{warning("This function is meant for use in interactive mode only.\n")}
+return(invisible(NULL))
+}
+
+GetPython27 = function(){
+if(interactive()){
+if(.Platform$OS.type=="windows"){
+if(require(BrailleR)){
+message("This command will download a file and save it to your hard drive.")
+installr::install.URL("https://www.python.org/ftp/python/2.7.11/python-2.7.11.msi", download_dir = getOption("BrailleR.Folder"), keep_install_file=TRUE)
+message("The installer file has been added to your MyBrailleR folder.")
+message("You can delete it at any time, but that will not uninstall the application.")
+}
+} else{warning("This function is for users running R under the Windows operating system.\n")}
+} else{warning("This function is meant for use in interactive mode only.\n")}
+return(invisible(NULL))
+}
+
+
+GetWxPython27 = function(){
+if(interactive()){
+if(.Platform$OS.type=="windows"){
+if(require(BrailleR)){
+message("This command will download a file and save it to your hard drive.")
+installr::install.URL("http://downloads.sourceforge.net/wxpython/wxPython3.0-win32-3.0.2.0-py27.exe", download_dir = getOption("BrailleR.Folder"), keep_install_file=TRUE)
+message("The installer file has been added to your MyBrailleR folder.")
+message("You can delete it at any time, but that will not uninstall the application.")
+}
+} else{warning("This function is for users running R under the Windows operating system.\n")}
+} else{warning("This function is meant for use in interactive mode only.\n")}
+return(invisible(NULL))
+}
+
+GetWriteR= function(UseGitHub = TRUE){
+message("This command is temporarily unavailable.")
+}
+
