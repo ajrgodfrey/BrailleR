ea0c8f9186b0f3c6d49b3ac5ec259d3e08c5007c
ajrgodfrey <a.j.godfrey@massey.ac.nz>
2016-06-21 14:14:24 +1200

    changes for v0.25.1

---
+++ R/CleanCSV.R
@@ -0,0 +1,17 @@
+
+CleanCSV= function(file){
+if(interactive()){
+for(ThisFile in file){
+cat(paste0(ThisFile, ": "))
+if(file.exists(ThisFile)){
+file.copy(ThisFile, paste0(ThisFile, ".bak"))
+for(FixString in c(", ", " ,", "\t,", ",\t")){ FindReplace(ThisFile, FixString, ",") }
+message("Done.")
+} # end ThisFile exists condition
+else{warning("The specified file does not exist.\n")}
+}# end for loop for files
+} # end interactive ondition
+else{ warning("This function is meant for use in interactive mode only.\n")}
+
+return(invisible(NULL))
+}
