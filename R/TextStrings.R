df62eb0a7f6b3452e201ec0420430898899b6772
ajrgodfrey <a.j.godfrey@massey.ac.nz>
2017-01-16 14:41:13 +1300

    changes for v0.25.6 (stuff for more accessible plots)

---
+++ R/TextStrings.R
@@ -0,0 +1,29 @@
+
+.BoxplotText = function(fivenum, outliers=NULL, horizontal=FALSE){
+Outs=length(outliers)
+TextOuts = ifelse(Outs==0, "no", as.character(Outs))
+ShortText = paste("Min", fivenum[1], "Lower quartile", fivenum[2], "Median", fivenum[3], "Upper quartile", fivenum[4], "Max", fivenum[5], "with", TextOuts, ifelse(Outs==1, "outlier.", "outliers."))
+
+Text1 = Text2 = Text3 = Text4 = Text5 = Text6 = ""
+
+        if (Outs>0) {
+          Text1 = paste(ifelse(Outs==1, "An outlier is", "Outliers are"), 'marked at:', outliers)
+}
+        Text2 = paste('The whiskers extend to', fivenum[1], 'and', fivenum[5],
+            'from the ends of the box, \nwhich are at', fivenum[2], 'and',
+            fivenum[4], '\n')
+        BoxLength = fivenum[4] - fivenum[2]
+        Text3 = paste('The median,', fivenum[3], 'is',
+            round(100 * (fivenum[3] - fivenum[2]) / BoxLength, 0),
+            '% from the', ifelse(horizontal, 'left', 'lower'),
+            'end of the box to the', ifelse(horizontal, 'right', 'upper'),
+            'end.\n')
+        Text4 = paste('The', ifelse(horizontal, 'right', 'upper'), 'whisker is',
+            round((fivenum[5] -
+                   fivenum[4]) / (fivenum[2] - fivenum[1]), 2),
+            'times the length of the', ifelse(horizontal, 'left', 'lower'),
+            'whisker.\n')
+
+ LongText = paste(Text1, Text2, Text3, Text4) 
+return(invisible(c(ShortText,LongText)))
+}
