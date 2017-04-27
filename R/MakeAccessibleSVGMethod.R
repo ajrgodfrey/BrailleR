de03bdfc790e1f64358fbe675ea6e28b643755c9
zorkow <volker.sorge@gmail.com>
2017-04-27 05:20:17 +0100

    Rewrites Timeseries SVG to split continuous graph into segments.

--- R/MakeAccessibleSVGMethod.R
+++ R/MakeAccessibleSVGMethod.R
@@ -28,6 +28,9 @@ MakeAccessibleSVG.histogram =
 MakeAccessibleSVG.tsplot =
     function(x, file = "test", view=interactive()) {
       svgfile = SVGThis(x, paste0(file, ".svg"))
+      if (x$Continuous) {
+        RewriteSVG(x, paste0(file, ".svg"))
+      }
       xmlfile = AddXML(x, paste0(file, ".xml"))
       if (view) {
           BrowseSVG(file=file, view=view)
