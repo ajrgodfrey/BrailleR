984e5aaa6cc208fedf54ea1bc1dea588998cd81a
ajrgodfrey <a.j.godfrey@massey.ac.nz>
2017-05-11 13:18:36 +1200

    changes for v0.25.7 (fixing scatter and fitted line plots)

--- R/ScatterPlot.R
+++ R/ScatterPlot.R
@@ -4,16 +4,16 @@ ScatterPlot = function(x, y=NULL, ...){
     MC <- match.call(expand.dots = TRUE)
     Out = list()
     Out$data = .CleanData4TwoWayPlot(x, y)
-    MC$xlab= ifelse(is.null(MC$xlab), as.character(MC$x), MC$xlab)
-    MC$ylab= ifelse(is.null(MC$ylab), as.character(MC$y), MC$ylab)
+    Out$ExtraArgs  = .GrabExtraArgs(MC)
+    Out$ExtraArgs$xlab =     MC$xlab= ifelse(is.null(MC$xlab), as.character(MC$x), MC$xlab)
+    Out$ExtraArgs$ylab =     MC$ylab= ifelse(is.null(MC$ylab), as.character(MC$y), MC$ylab)
+    Out = .checkTextLabels(MC, Out)
     MC[[1L]] <- quote(graphics::plot)
     MC$x <- Out$data$x
     MC$y <- Out$data$y
     Out$graph <- eval(MC, envir=parent.frame())
-    Out$ExtraArgs  = .GrabExtraArgs(MC)
     Out$par = par()
     class(Out) = "scatterplot"
-    Out = .checkTextLabels(MC, Out)
     Out=Augment(Out)
     return(invisible(Out))
 }
@@ -23,20 +23,22 @@ FittedLinePlot = function(x, y, line.col=2, ...){
     MC <- match.call(expand.dots = TRUE)
     Out = list()
     Out$data = .CleanData4TwoWayPlot(x, y)
-    MC$xlab= ifelse(is.null(MC$xlab), as.character(MC$x), MC$xlab)
-    MC$ylab= ifelse(is.null(MC$ylab), as.character(MC$y), MC$ylab)
+    Out$fittedline = list(coef = coef(lm(y~x, data=Out$data)),
+             col = line.col)
+    Out$ExtraArgs  = .GrabExtraArgs(MC)
+    Out$ExtraArgs$xlab  = MC$xlab = ifelse(is.null(MC$xlab), as.character(MC$x), MC$xlab)
+    Out$ExtraArgs$ylab  =     MC$ylab= ifelse(is.null(MC$ylab), as.character(MC$y), MC$ylab)
+    Out = .checkTextLabels(MC, Out)
     MC[[1L]] <- quote(graphics::plot)
     MC$x <- Out$data$x
     MC$y <- Out$data$y
-    Out$graph <- eval(MC, envir=parent.frame())
-    Out$ExtraArgs  = .GrabExtraArgs(MC)
+    MC$line.col=NULL
+#    Out$graph <- eval(MC, envir=parent.frame())
+    Out$graph <- do.call(graphics::plot, as.list(MC[-1]))
     Out$par = par()
     class(Out) = c("fittedlineplot", "scatterplot")
-    Out = .checkTextLabels(MC, Out)
     Out=Augment(Out)
-    Out$fittedline$coef = coef(lm(y~x, data=Out$data))
-    Out$fittedline$col = line.col
-    abline(Out$fittedline$coef, col=line.col)
+    do.call(abline, Out$fittedline)
     return(invisible(Out))
 }
 
@@ -72,7 +74,7 @@ Pars=x$ExtraPars
 fitline = x$fittedline
 x = .RemoveExtraGraphPars(x)
 suppressWarnings(do.call(plot, c(x, Pars)))
-    abline(fitline$coef, col=fitline$col)
+    do.call(abline, fitline)
 return(invisible(NULL))
 }
 
@@ -80,7 +82,7 @@ print.fittedlineplot = plot.fittedlineplot
 print.scatterplot = plot.scatterplot
 
 .RemoveExtraGraphPars = function(x){
-ToRemoveBrailleRBits = c("xTicks", "yTicks", "par", "GroupSummaries", "Continuous", "coef", "data", "fittedline", "main", "sub", "xlab", "ylab", "ExtraArgs")
+ToRemoveBrailleRBits = c("xTicks", "yTicks", "par", "GroupSummaries", "Continuous", "coef", "data", "fittedline", "main", "sub", "xlab", "ylab", "ExtraArgs", "line.col")
 ToRemoveBasePar = c("cin", "cra", "csi", "cxy", "din", "page")
 ToRemove = c(ToRemoveBasePar, ToRemoveBrailleRBits)
 x2 = x[setdiff(names(x), ToRemove)]
@@ -89,7 +91,7 @@ return(invisible(x2))
 }
 
 .GrabExtraArgs = function(x){
-ToRemove = c("", "x", "y", "main", "xlab", "ylab", "sub")
+ToRemove = c("", "x", "y", "main", "xlab", "ylab", "sub", "line.col")
 ExtraArgs = as.list(x[setdiff(names(x), ToRemove)])
 return(invisible(ExtraArgs))
 }
