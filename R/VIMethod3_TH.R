d7fbe2afea9fe990b467f1ebf77c4c60881c3eb3
ajrgodfrey <a.j.godfrey@massey.ac.nz>
2017-05-15 16:45:46 +1200

    changes for v0.25.7 (almost time for a ninor version number change)

--- R/VIMethod3_TH.R
+++ R/VIMethod3_TH.R
@@ -1,8 +1,14 @@
 VI.ggplot =
     function(x, ...) {
+      TitleText = ifelse(is.null(x$labels$title), "This untitled chart;\n",
+              paste0('This chart titled ', .getTextGGTitle(x), ';\n'))
+      SubtitleText = ifelse(is.null(x$labels$subtitle), "has no subtitle;\n",
+              paste0('has the subtitle: ', .getTextGGSubtitle(x), ';\n'))
+      CaptionText = ifelse(is.null(x$labels$caption), "and no caption.\n",
+              paste0('and the caption: ', .getTextGGCaption(x), '.\n'))
+
       txt =
-          paste0(
-              'The chart titled ', BrailleR::InQuotes(x$labels$title), ';\n',
+          paste0(TitleText, SubtitleText, CaptionText,
               'with x-axis ', BrailleR::InQuotes(x$labels$x), ' labeled from ',
               ggplot_build(x)$panel$ranges[[1]]$x.labels[1], ' to ',
               tail(ggplot_build(x)$panel$ranges[[1]]$x.labels, n = 1),
@@ -10,7 +16,7 @@ VI.ggplot =
               ggplot_build(x)$panel$ranges[[1]]$y.labels[1], ' to ',
               tail(ggplot_build(x)$panel$ranges[[1]]$y.labels, n = 1), ';\n')
 
-      if ('colour' %in% attributes(x$labels)$names) {
+      if ('colour' %in% names(x$labels)) {
         txt = paste0(txt, '\nColour is used to represent ',
                      BrailleR::InQuotes(x$labels$colour))
 
@@ -20,10 +26,49 @@ VI.ggplot =
                      paste(levels(x$data[[x$labels$colour]]), collapse = ', '),
                      '.\n')
         } else {
-          txt = paste0(txt, , ';\n')
+          txt = paste0(txt, ';\n')
         }
       }
       cat(txt)
       return(invisible(NULL))
     }
+
+.getTextGGTitle = function(x){
+if(is.null(x$labels$title)){
+text = "no label"
+} else {
+text =  BrailleR::InQuotes(x$labels$title)
+}
+return(invisible(text))
+}
+
+.getTextGGSubtitle = function(x){
+if(is.null(x$labels$subtitle)){
+text = "no label"
+} else {
+text =  BrailleR::InQuotes(x$labels$subtitle)
+}
+return(invisible(text))
+}
+
+.getTextGGCaption = function(x){
+if(is.null(x$labels$caption)){
+text = "no label"
+} else {
+text =  BrailleR::InQuotes(x$labels$caption)
+}
+return(invisible(text))
+}
+
+
+.getTextGGXLab = function(x){
+text = BrailleR::InQuotes(x$labels$x)
+}
+
+
+
+.getTextGGYLab = function(x){
+text = BrailleR::InQuotes(x$labels$y)
+}
+
 
