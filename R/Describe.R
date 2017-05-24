2a06a4bd27a15cc68ecaf945edb35a2419a360c0
ajrgodfrey <a.j.godfrey@massey.ac.nz>
2017-05-24 14:51:23 +1200

    changes for v0.25.7 (implementing graph alterations)

--- R/Describe.R
+++ R/Describe.R
@@ -2,11 +2,13 @@
 # help page is shared with VI()
 
 Describe = 
-    function(x, VI=FALSE){
+    function(x, VI=FALSE, ...){
       UseMethod("Describe")
     }
 
-.CommonAxisStyleText ="As with most graphs created by the base graphics package, the axes do not join at the bottom left corner and are separated from the area where the data are being plotted. Tick marks are automatically chosen for the data, and the axes may not extend past the ends of variables being plotted."
+.CommonAxisStyleText ="As with most graphs created by the base graphics package, the axes do not join at the bottom left corner and are separated from the area where the data are being plotted."
+
+.CommonTickMarkText = "Tick marks are automatically chosen for the data, and the axes may not extend past the ends of variables being plotted."
 
 print.description = 
     function(x, ...){
@@ -15,7 +17,7 @@ print.description =
     }
 
 Describe.default = 
-    function(x, VI=FALSE){
+    function(x, VI=FALSE, ...){
         if(VI) VI(x)
         title = "No description of this type of object has been written at this time."
         general = ""
@@ -25,10 +27,10 @@ Describe.default =
         return(Out)
     }
 Describe.histogram = 
-    function(x, VI=FALSE){
+    function(x, VI=FALSE, ...){
         if(VI) VI(x)
         title = "A histogram created using the base graphics package."
-        general = paste("A histogram uses rectangles to represent the counts or relative frequencies of observations falling in each subrange of the numeric variable being investigated. The rectangles are standing side by side with their bottom end at the zero mark of the vertical axis. The widths of the histogram are usually constant, but this can be altered by the user. A sighted person uses the areas of the rectangles to help determine the overall shape of the distribution, the presence of gaps in the data, and any outliers that might be present.", .CommonAxisStyleText, "The vertical axis for frequency always starts at zero.") 
+        general = paste("A histogram uses rectangles to represent the counts or relative frequencies of observations falling in each subrange of the numeric variable being investigated. The rectangles are standing side by side with their bottom end at the zero mark of the vertical axis. The widths of the rectangles are usually constant, but this can be altered by the user. A sighted person uses the heights and therefore the areas of the rectangles to help determine the overall shape of the distribution, the presence of gaps in the data, and any outliers that might be present.", .CommonAxisStyleText, .CommonTickMarkText, "The vertical axis for frequency always starts at zero.") 
         RHints = ""
         Out = list(title = title, general = general, RHints = RHints)
         class(Out) = "description"
