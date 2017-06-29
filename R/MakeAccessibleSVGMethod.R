271890e4d0c3178820dc4c4afcbaaa669db2fac6
ajrgodfrey <a.j.godfrey@massey.ac.nz>
2017-06-29 12:27:02 +1200

    changes for v0.25.7 (implementing graph alterations)

--- R/MakeAccessibleSVGMethod.R
+++ R/MakeAccessibleSVGMethod.R
@@ -14,7 +14,7 @@ MakeAccessibleSVG.default =
       return(invisible(NULL))
 }
 
-MakeAccessibleSVG.histogram =
+MakeAccessibleSVG.histogram = MakeAccessibleSVG.scatterplot =
     function(x, file = "test", view=interactive(), ...) {
       svgfile = SVGThis(x, paste0(file, ".svg"))
       xmlfile = AddXML(x, paste0(file, ".xml"))
