c3ada370244eb5fcb01ad72a8375e275fc326a73
ajrgodfrey <a.j.godfrey@massey.ac.nz>
2018-06-11 17:46:13 +1200

    v0.29.0 adding pdf2html() functionality

--- R/Require.R
+++ R/Require.R
@@ -1,7 +1,7 @@
 
 Require = function(pkg){
 pkg = as.character(substitute(pkg))
-if(!suppressPackageStartupMessagesbase::require(pkg, character.only=TRUE, warn.conflicts=FALSE))){
+if(!suppressPackageStartupMessages(base::require(pkg, character.only=TRUE, warn.conflicts=FALSE))){
 utils::chooseCRANmirror(ind=1)
 utils::install.packages(pkg)
 }
