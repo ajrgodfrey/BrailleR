00eee95dc448f252fc22c6aea592f11fb2b4f75d
ajrgodfrey <a.j.godfrey@massey.ac.nz>
2020-04-25 15:03:44 +1200

    v0.31.0 added RemoveBOM()

--- R/RemoveBOM.R
+++ R/RemoveBOM.R
@@ -1,10 +1,10 @@
-RemoveBOM = function(File){
-FileLines = readLines(File)
+RemoveBOM = function(file){
+FileLines = readLines(file)
 Line1 = FileLines[1]
 
 if(nchar(Line1)==6 & nchar(strsplit(Line1,"---")) ==3){
 FileLines[1] = "---"
-writeLines(FileLines, con=File)
+writeLines(FileLines, con=file)
 message("BOM removed from file")
 return(invisible(TRUE))
 }
