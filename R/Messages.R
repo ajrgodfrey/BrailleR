0c977326d9c43a3faa48e0386416bc6929d12749
ajrgodfrey <ajgodfre@massey.ac.nz>
2022-12-05 07:41:10 +1300

    v0.33.4 fixing more messages

--- R/Messages.R
+++ R/Messages.R
@@ -1,4 +1,3 @@
-# add filenames to messages for file creation
 
 
 .BlankMSG =     function() {
@@ -13,27 +12,22 @@ return(invisible(NULL))
 
 
         .AnswerQuestions=function(){
-message("You will be asked to enter answers for a series of questions.\nHit <enter> to use the default shown in parentheses.")
+message("You will be asked to enter answers for a series of questions.Hit <enter> to use the default shown in parentheses.")
 return(invisible(NULL))
 }
 
 .AuthorName =     function() {
-message("\nEnter the name you want to use for authoring content. (",
+message("Enter the name you want to use for authoring content. (",
                 getOption("BrailleR.Author"), ")")
 return(invisible(NULL))
-    }
+}
 
 .AutoLoadBrailleR  =     function() {
 message("The BrailleR package will be automatically loaded on startup in this working directory.")
 return(invisible(NULL))
-    }
-
-
-
-.CannotSeeWxPython = function(){
-message("Python cannot see the necessary wx module.\nYou need to get that fixed.")
-return(invisible(NULL))
 }
+
+
 
 
 
@@ -50,7 +44,7 @@ return(invisible(NULL))
 .ConsultHelpPage  =     function() {
 message("Consult the help page for guidance on using these files in Windows Explorer.")
 return(invisible(NULL))
-    }
+}
 
 
 .DeleteAnytime  = function(){
@@ -63,65 +57,60 @@ message("Done.")
 return(invisible(NULL))
 }
 
-.DownloadAFile = function(){
-message("This command will download a file and save it to your hard drive.")
+
+.FileCreated =     function(file=NULL, where="in your MyBrailleR directory.") {
+NewFile = ifelse(is.null(file), "A new file", file)
+message(NewFile, " has been created ", where)
 return(invisible(NULL))
 }
 
-
-.FileCreated =     function(file=NULL, where="in your MyBrailleR directory.") {
-NewFile = ifelse(is.null(file), "", file)
-message(NewFile, " has been created ", where)
-return(invisible(NULL))
-    }
-
 .FileUpdated =     function(file=NULL, where="in your MyBrailleR directory.") {
-NewFile = ifelse(is.null(file), "", file)
+NewFile = ifelse(is.null(file), "The specified", file)
 message(NewFile, " has been updated ", where)
 return(invisible(NULL))
-    }
+}
 
 
 
 .InstallPython =     function() {
 message("You could use GetPython3() and GetWxPython3() to help install them.")
 return(invisible(NULL))
-    }
+}
 
 .MoveOntoPath=     function() {
 message("These files need to be moved to a folder that is on your system path.")
 return(invisible(NULL))
-    }
+}
 
 
 .NewFile =     function(file=NULL) {
 NewFile = ifelse(is.null(file), "", file)
 message(NewFile, " has been created in your working directory.")
 return(invisible(NULL))
-    }
+}
 
 .NoActionTaken =     function() {
 message("No action taken.")
 return(invisible(NULL))
-    }
+}
 
 
 
 .NoSeePython = function(){
-message("Python cannot be seen on your system.\nIf it is installed, then you may need to ensure your system settings are correct.")
+message("Python cannot be seen on your system.If it is installed, then you may need to ensure your system settings are correct.")
 return(invisible(NULL))
 }
 
 .NothingDoneGraph =     function() {
 message("Nothing done to augment this graph object.")
 return(invisible(NULL))
-    }
+}
 
 .NoVIMethod =     function() {
-      message("There is no specific method written for  this type of object.\n")
-      message("You might try to use the print() function on the object or the str() command to investigate its contents.\n")
-      return(invisible(NULL))
-    }
+      message("There is no specific method written for  this type of object.")
+message("You might try to use the print() function on the object or the str() command to investigate its contents.")
+return(invisible(NULL))
+}
 
 
 .OptionLocal =     function() {
@@ -143,7 +132,7 @@ return(invisible(NULL))
 
 
 .OriginalDefaults =     function() {
-message("You have reset all preferences to the original package defaults.\n")
+message("You have reset all preferences to the original package defaults.")
 return(invisible(NULL))
 }
 
@@ -152,12 +141,19 @@ return(invisible(NULL))
 VersionString = system2("python", "--version", stdout=TRUE, stderr=TRUE)
 message("Your system is using ", VersionString, "")
 return(invisible(NULL))
-    }
+}
+
+.QuartoVersion =     function() {
+VersionString = system2("quarto", "--version", stdout=TRUE, stderr=TRUE)
+message("Your system is using ", VersionString, "")
+return(invisible(NULL))
+}
+
 
 .SavedInPath =     function() {
 message("These details are saved in path.txt for reference.")
 return(invisible(NULL))
-    }
+}
 
 
 
@@ -166,11 +162,6 @@ return(invisible(NULL))
 .SVGAndXMLMade =     function() {
 message("SVG and XML files created successfully.")
 return(invisible(NULL))
-    }
+}
 
-
-.TempUnavailable =     function() {
-  message("This command is temporarily unavailable.")
-return(invisible(NULL))
-    }
 
--- R/Warnings.R
+++ R/Warnings.R
@@ -1,76 +1,83 @@
 
 .BlankWarning =     function() {
-warning("\n")
+warning("")
 return(invisible(NULL))
 }
 
-.DeprecatedFunction =     function() {
-warning("This function has been deprecated.\n")
+.CannotSeeWxPython = function(){
+warning("Python cannot see the necessary wx module.You need to get that fixed.")
 return(invisible(NULL))
-    }
+}
 
 
+.DeprecatedFunction =     function() {
+warning("This function has been deprecated.")
+return(invisible(NULL))
+}
+
+
+
+
+.DownloadAFile = function(){
+warning("This command will download a file and save it to your hard drive.")
+return(invisible(NULL))
+}
 
 
 .ExpectingText =     function() {
-warning("A text string was expected. No action taken.\n")
+warning("A text string was expected. No action taken.")
 return(invisible(NULL))
 }
 
 .ExperimentalFunction =     function() {
-warning("This function is purely experimental. Use it at your own risk.\n")
+warning("This function is purely experimental. Use it at your own risk.")
 return(invisible(NULL))
 }
 
 .FileDoesNotExist =     function(file=NULL) {
-warning(ifelse(is.null(file), "The specified file", file),  " does not exist.\n")
+warning(ifelse(is.null(file), "The specified file", file),  " does not exist.")
 return(invisible(NULL))
-    }
+}
 
 .FileExists =     function(file) {
-warning(file, "already exists.\n")
+warning(file, "already exists.")
 return(invisible(NULL))
-    }
+}
 
 
 
-##kill this one
-.FileNotFound =     function(file=NULL) {
-warning("The specified file does not exist.\n")
+
+
+.FolderNotFound =     function(folder=NULL) {
+warning(ifelse(is.null(folder), "The specified folder", folder),  " does not exist.")
 return(invisible(NULL))
-    }
-
-
-.FolderNotFound =     function() {
-warning("The specified folder does not exist.\n")
-return(invisible(NULL))
-    }
+}
 
 
 .InteractiveOnly =     function() {
-warning("This function is meant for use in interactive mode only.\n")
+warning("This function is meant for use in interactive mode only.")
 return(invisible(NULL))
-    }
+}
 
 
 .NeedsPython =     function() {
- warning("This function requires installation of Python 3.0 or above.\n")
+warning("This function requires installation of Python 3.0 or above.")
 return(invisible(NULL))
-    }
+}
 
 .NeedsWX =     function() {
-warning("This function requires an installation of Python and wxPython.\n")
+warning("This function requires an installation of Python and wxPython.")
 return(invisible(NULL))
-    }
+}
 
 
 .NoConversion =     function() {
-warning("There was a problem and no conversion was possible.\n")
+warning("There was a problem and no conversion was possible.")
 return(invisible(NULL))
-    }
+}
 
 .NoGraphicsDevice =     function() {
-warning("There is no current graphics device to investigate.\n")
+warning("There is no current graphics device to investigate.")
 return(invisible(NULL))
 }
 
@@ -78,19 +85,24 @@ return(invisible(NULL))
 
 
 .OverWriteNeeded =     function(file=NULL) {
-warning("No action has been taken. Use `Overwrite=TRUE` if you want to replace it.\n")
+warning("No action has been taken. Use `Overwrite=TRUE` if you want to replace it.")
 return(invisible(NULL))
-    }
+}
 
 
-.PkgNotFound =     function() {
-warning("The specified package is not installed.\n")
+.PkgNotFound =     function(Pkg=NULL) {
+warning("The ", ifelse(is.null(Pkg), "specified package", Pkg),  " does not exist.")
 return(invisible(NULL))
-    }
+}
+
+.TempUnavailable =     function() {
+message("This command is temporarily unavailable.")
+return(invisible(NULL))
+}
 
 
 
 .WindowsOnly =     function() {
-warning("This function is for users running R under the Windows operating system.\n")
+warning("This function is for users running R under the Windows operating system.")
 return(invisible(NULL))
-    }
+}
