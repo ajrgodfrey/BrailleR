c23fac61ea94b0652e29ad65f3ad7c3d7881dce5
James <1jamesthompson1@gmail.com>
2022-12-07 12:45:45 +1300

    0.33.4 Fix build errors with TSPlot
    Fixed errors with RD help file
    Added imports to the NAMESPACE
    Update tsplot code a bit for it to work with ts data correctly and handle the labels and title correctly.

--- R/TSPlot.R
+++ R/TSPlot.R
@@ -3,13 +3,9 @@
 TimeSeriesPlot = function(.data, x, time=NULL, base=FALSE, ...){
   Out = list()
   MC = match.call(expand.dots = TRUE)
-  MC$ylab= ifelse(is.null(MC$ylab), as.character(MC$x), MC$ylab)
-  if (length(MC$sub) > 0) Out$sub = as.character(MC$sub)
-  if (length(MC$xlab) > 0) Out$xlab = as.character(MC$xlab) else {Out$xlab = "Time"}
-  if (length(MC$ylab) > 0) Out$ylab = as.character(MC$ylab)
   if(base){
     Out = list(x=as.ts(x))
-
+    MC$ylab= ifelse(is.null(MC$ylab), as.character(MC$x), MC$ylab)
     MC[[1L]] <- quote(graphics::plot)
     MC$x <- Out$x
     
@@ -18,16 +14,31 @@ TimeSeriesPlot = function(.data, x, time=NULL, base=FALSE, ...){
     Out$par = par()
     class(Out) = "tsplot"
     if (length(MC$main) > 0) Out$main = as.character(MC$main) else {Out$main = ""}
+    if (length(MC$sub) > 0) Out$sub = as.character(MC$sub)
+    if (length(MC$xlab) > 0) Out$xlab = as.character(MC$xlab) else {Out$xlab = "Time"}
+    if (length(MC$ylab) > 0) Out$ylab = as.character(MC$ylab)
     Out=Augment(Out)
   }
   else{ ## do it in ggplot2
+    print(MC$ylab)
+    MC$ylab = ifelse(is.null(MC$ylab), tail(strsplit(as.character(MC$x), "$"),n=1), MC$ylab)
+    if (length(MC$sub) > 0) Out$sub = as.character(MC$sub)
+    if (length(MC$xlab) > 0) Out$xlab = as.character(MC$xlab) else {Out$xlab = "Time"}
+    if (length(MC$ylab) > 0) Out$ylab = as.character(MC$ylab)
+    if (length(MC$main) > 0) Out$main = as.character(MC$main)
+    
+    #Special care has to be taken if it is a dataframe
     if (is.ts(.data)) {
+      #Sort out data frame
       .data = data.frame(Y=as.matrix(.data), date=time(.data))
       time = .data$date
       x = .data$Y
+      #Get correct ylab
+      Out$ylab = ifelse(is.null(MC$ylab[[1]]), deparse(MC$.data), MC$ylab)
     }
-    if (length(MC$main) > 0) Out$main = as.character(MC$main)
+    
     if (is.null(time)) time = 1:length(x)
+    
     Out = ggplot(.data, aes(y=x, x=time)) +
       geom_line() +
       ylab(Out$ylab) + xlab(Out$xlab) +
