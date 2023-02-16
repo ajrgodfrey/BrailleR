18cf64a0f7d15e2564af59440b3d1bdf6c30bff8
ajrgodfrey <ajgodfre@massey.ac.nz>
2023-02-16 19:23:28 +1300

    v0.99.0 late fixes before major release

--- R/PandocAll.R
+++ R/PandocAll.R
@@ -6,11 +6,8 @@ for(i in FileList){
 Outfile = sub(paste0(".", intype), paste0(".", outtype),  i)
 if(file.mtime(i) > file.mtime(Outfile)| !file.exists(Outfile)) {
 shell(paste0('pandoc -s "', i, '" -o "', Outfile, '"'))
-.FileUpdated(file=Outfile, where="in your current working directory."))
+.FileUpdated(file=Outfile, where="in your current working directory.")
 }
 }
-
-
-
 return(invisible(TRUE))
 }
