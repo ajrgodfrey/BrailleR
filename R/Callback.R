914c7e93edf20da3bcd7614c9e3e15150379fe9a
ajrgodfrey <a.j.godfrey@massey.ac.nz>
2023-09-02 04:50:54 +1200

    callback

---
+++ R/Callback.R
@@ -0,0 +1,35 @@
+
+SessionLog <- function(file = "") {
+    df <- .get_session_log()
+    mapply(function(expr, pr) cat(c(paste(">", paste(expr, collapse = "\n")), pr, ""), sep = "\n", file = file),
+           expr = df$expr,
+           pr = df$pr_output)
+    invisible(NULL)
+}
+
+
+.callback <- function(expr, value, success, printed, env) {
+    if(!success)
+        return(TRUE)
+
+    if(printed)
+        prout <- tryCatch(capture.output(print(value)), error = function(e) "error during callback")
+    else
+        prout <- ""
+        rw <- data.frame(expr = I(list(deparse(expr))),
+                         val_class = I(list(class(value))),
+                         pr_output = I(list(prout)))
+    if(exists("session_log", env))
+        env$session_log <- c(env$session_log,list(rw))
+    else
+        env$session_log <- list(rw)
+    TRUE
+}
+
+
+.get_session_log <- function() {
+    do.call(rbind, BrailleR$session_log)
+}
+
+
+addTaskCallback(.callback, data = BrailleR, name = "BrailleR_logger")
